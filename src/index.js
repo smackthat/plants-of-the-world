67845b012bec38264b6269c7d30dc5a1b68b3026
Jdog <jstenberg86@gmail.com>
2021-02-02 20:42:34 +0200

    Init commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,160 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as d3geo from 'd3-geo';
+import * as d3fetch from 'd3-fetch';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//#region Main thang
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Globe extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      geographies: this.props.geoJson.features
+    };
+  }
+
+  render() {
+
+    let projection = d3geo.geoOrthographic()
+      .rotate([0, 0])
+      .clipAngle(90)
+      .fitSize([this.props.size, this.props.size], this.props.geoJson);
+
+    console.log(projection);
+
+    // let geoGenerator = d3geo.geoPath()
+    //   .projection(projection);
+
+    // let pathString = geoGenerator(this.props.geoJson);
+
+    return <svg width={this.props.size} height={this.props.size}>
+      {/* <path d={pathString}></path>
+      <path className="water" d={pathString}></path> */}
+      <g className="regions">
+        {this.state.geographies.map((d, i) =>
+          <path
+            key={d.properties.LEVEL3_COD}
+            id={d.properties.LEVEL3_COD}
+            d={d3geo.geoPath().projection(projection)(d)}
+            className="region"
+          >
+          </path>
+        )}
+      </g>
+    </svg>
+  }
+}
+
+//#endregion Main thang
+
+
+//#region Sample methods
+// class Square extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       value: null
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+// class Board extends React.Component {
+
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       squares: Array(9).fill(null)
+//     }
+//   }
+
+//   handleClick(i) {
+//     const squares = this.state.squares.slice();
+//     squares[i] = 'X';
+//     this.setState({ squares: squares });
+//   }
+
+//   renderSquare(i) {
+//     return <Square
+//       value={this.state.squares[i]}
+//       onClick={() => this.handleClick(i)}
+
+//     />;
+//   }
+
+//   render() {
+//     const status = 'Next player: X';
+
+//     return (
+//       <div>
+//         <div className="status">{status}</div>
+//         <div className="board-row">
+//           {this.renderSquare(0)}
+//           {this.renderSquare(1)}
+//           {this.renderSquare(2)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(3)}
+//           {this.renderSquare(4)}
+//           {this.renderSquare(5)}
+//         </div>
+//         <div className="board-row">
+//           {this.renderSquare(6)}
+//           {this.renderSquare(7)}
+//           {this.renderSquare(8)}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// class Game extends React.Component {
+//   render() {
+//     return (
+//       <div className="game">
+//         <div className="game-board">
+//           <Board />
+//         </div>
+//         <div className="game-info">
+//           <div>{/* status */}</div>
+//           <ol>{/* TODO */}</ol>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+//#endregion Sample methods
+
+// ========================================
+
+d3fetch.json('./assets/level3.json').then((json) => {
+
+  console.log(json);
+  // console.log('ERR: ', err);
+
+  ReactDOM.render(
+    <Globe
+      size={600}
+      geoJson={json}
+    >
+    </Globe>,
+    document.getElementById('root')
+  );
+});
+
+
